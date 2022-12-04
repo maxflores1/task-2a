76083efcb35ee6079e000493387ae91269836675
maxflores1 <118218453+maxflores1@users.noreply.github.com>
2022-12-03 22:09:30 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let mySprite: Sprite = null
+game.onUpdateInterval(500, function () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . 1 . . . 
+        . 5 . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 1 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 5 
+        . . . . . 5 . . . . . 1 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Projectile)
+    mySprite.setPosition(0, randint(0, scene.screenHeight()))
+    mySprite.setVelocity(50, 50)
+})
